88c0e08a1423903e2db216941d709021ebbb2832
kawayi <kawayi_rendroid@163.com>
2018-10-26 06:49:01 +0000

    [spiral-matrix-II] 50%, pass on num of 3, fail at 4,5

--- cmake_ongoing/leecode/question1.cpp
+++ cmake_ongoing/leecode/question1.cpp
@@ -1,7 +1,15 @@
 #include<iostream>
 #include <map>
+#include <vector>
 #include <string.h>
+#include <stdio.h>
 using namespace std;
+#define OPEN_LOG
+#ifdef OPEN_LOG
+#define leet_logv printf
+#else
+#define leet_logv 
+#endif
 #if 0
 static int* twoSum(int nums[], int target) 
 {   
@@ -34,8 +42,75 @@ void test_two_sum()
 }
 #endif
 
+
+class Solution {
+public:
+    vector< vector<int> > generateMatrix(int n) {
+        int d=n-1;
+        int x=n-d-1,y=0;
+        int num=1;
+        vector< vector<int> > res;
+        res.resize(n);
+        for(int i=0;i<n;i++) res[i].resize(n);
+        
+        while(d>0){
+            leet_logv("d:%d,start x,y:%d,%d\n",d,x,y);
+            
+            for(int i=0;i<d;i++) {
+                res[x][y+i]=num++;
+                leet_logv("res[%d][%d]:%d\n",x,y+i,res[x][y+i]);
+            }
+            y+=d;
+            leet_logv("finish 1st,x,y:%d,%d",x,y);
+            for(int i=0;i<d;i++) {
+                res[x+i][y]=num++;
+                leet_logv("res[%d][%d]:%d\n",x+i,y,res[x+i][y]);
+            }
+            x+=d;
+            leet_logv("finish 2ed,x,y:%d,%d",x,y);
+            for(int i=0;i<d;i++) {
+                res[x][y-i]=num++;
+                leet_logv("res[%d][%d]:%d\n",x,y-i,res[x][y-i]);
+            }
+            y-=d;
+            leet_logv("finish 3rd,x,y:%d,%d",x,y);
+            for(int i=0;i<d;i++) {
+                res[x-i][y]=num++;
+                leet_logv("res[%d][%d]:%d\n",x-i,y,res[x-i][y]);
+            }
+            leet_logv("finish 4th,x,y:%d,%d",x,y);
+            x-=d;
+            d-=2;
+        }
+        if(d==0) res[n/2][n/2]=num;
+        leet_logv("res[%d][%d]:%d\n",x,y,res[x][y]);
+        return res;
+    }
+};
+
+static void spiral_matrix_ii(int n)
+{
+    Solution a;
+    vector< vector<int> > vec;
+    vector<int>::iterator it;
+    vector<vector<int> >::iterator iter;
+    vector<int> vec_tmp;
+    printf("[spiral_matrix_ii]: %d",n);
+    vec=a.generateMatrix(n);
+    
+    for(iter = vec.begin(); iter != vec.end(); iter++)
+    {
+        vec_tmp = *iter;
+        for(it = vec_tmp.begin(); it != vec_tmp.end(); it++)
+            cout << *it << " ";
+        cout << endl;
+    }    
+    
+}
 void leet_question_array()
 {
     cout<<"leet_question1"<<endl;
-    //test_two_sum();
+    spiral_matrix_ii(3);
+    spiral_matrix_ii(4);
+    spiral_matrix_ii(5);
 }
