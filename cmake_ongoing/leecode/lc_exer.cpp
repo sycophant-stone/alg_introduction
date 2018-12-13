6dc93790349a7c3d0ba21125c21c6eaf3cb508fb
kawayi <kawayi_rendroid@163.com>
2018-12-13 10:57:36 +0000

    add lc#299 bulls and cows

--- cmake_ongoing/leecode/lc_exer.cpp
+++ cmake_ongoing/leecode/lc_exer.cpp
@@ -361,7 +361,7 @@ static void lc41__FirstMissingPositive_case()
 // lc#299 
 //这道题提出了一个叫公牛母牛的游戏，其实就是之前文曲星上有的猜数字的游戏，有一个四位数字，你猜一个结果，然后根据你猜的结果和真实结果做对比，提示有多少个数字和位置都正确的叫做bulls，还提示有多少数字正确但位置不对的叫做cows，根据这些信息来引导我们继续猜测正确的数字。这道题并没有让我们实现整个游戏，而只用实现一次比较即可。给出两个字符串，让我们找出分别几个bulls和cows。这题需要用哈希表，来建立数字和其出现次数的映射。我最开始想的方法是用两次遍历，第一次遍历找出所有位置相同且值相同的数字，即bulls，并且记录secret中不是bulls的数字出现的次数。然后第二次遍历我们针对guess中不是bulls的位置，如果在哈希表中存在，cows自增1，然后映射值减1，参见如下代码：
 
-static void bullsandcows(string secret,string guess)
+static void bullsandcows_I(string secret,string guess)
 {
     int m[256];
     int bulls=0,cows=0;
@@ -382,11 +382,30 @@ static void bullsandcows(string secret,string guess)
     
 }
 
+static void bullsandcows_II(string secret,string guess)
+{
+    int m[256];
+    int bulls=0,cows=0;
+    for(int i=0;i<secret.size();i++){
+        if(secret[i]==guess[i]){
+            bulls++;
+        }else{
+            // secret和guess不是同一个的时候,记为cows
+            if(m[secret[i]]++<0) cows++; // 之前就有guess做减了.
+            if(m[guess[i]]-->0) cows++;
+        }
+    }
+    LC_LOGI("find! %d bulls, %d cows\n",bulls,cows);
+}
+
+
 static void lc299__BullsandCows_case()
 {
     string secret="1807";
     string guess="7810";
-    bullsandcows(secret,guess);
+    bullsandcows_I(secret,guess);
+    bullsandcows_II(secret,guess);
+    
     
 }
 
