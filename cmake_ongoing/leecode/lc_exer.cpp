13fb26947e7855ada70d95004e024266fbcc0838
kawayi <kawayi_rendroid@163.com>
2018-12-14 07:22:07 +0000

    lc#134 gas station

--- cmake_ongoing/leecode/lc_exer.cpp
+++ cmake_ongoing/leecode/lc_exer.cpp
@@ -409,6 +409,46 @@ static void lc299__BullsandCows_case()
     
 }
 
+/****************************************************************************************/
+/****************************************************************************************/
+/****************************************************************************************/
+// lc#134
+// 我们首先要知道能走完整个环的前提是gas的总量要大于cost的总量，这样才会有起点的存在。假设开始设置起点start = 0, 并从这里出发，如果当前的gas值大于cost值，就可以继续前进，此时到下一个站点，剩余的gas加上当前的gas再减去cost，看是否大于0，若大于0，则继续前进。当到达某一站点时，若这个值小于0了，则说明从起点到这个点中间的任何一个点都不能作为起点，则把起点设为下一个点，继续遍历。当遍历完整个环时，当前保存的起点即为所求。
+
+class GasStation{
+    public:
+    int gas_station(vector<int> &gas, vector<int> cost){
+        int total=0, sum=0, start=0;
+        printf("gas:\n");
+        print_int_vector(gas);
+        printf("cost:\n");
+        print_int_vector(cost);
+        for(int i=0;i<gas.size();i++){
+            total+=gas[i]-cost[i]; // 跑完所有站的剩余量(可以为负值).
+            sum+=gas[i]-cost[i];
+            if(sum<0){ // 只能从下一站开始跑.
+                start=i+1;
+                sum=0;
+            }
+        }
+        return (total < 0) ? -1 : start;
+    }
+};
+
+static void lc134__GasStation_case()
+{
+    int start=0;
+    vector<int> gas  = {1,2,3,4,5};
+    vector<int> cost = {3,4,5,1,2};
+    GasStation gass;
+    start=gass.gas_station(gas,cost);
+    LC_LOGI("find! start:%d\n",start);
+    gas  = {2,3,4};
+    cost = {3,4,3};
+    start=gass.gas_station(gas,cost);
+    LC_LOGI("find! start:%d\n",start);
+}
+
 void lc_entry()
 {
 //    spiral_matrix_ii_case();
@@ -417,5 +457,6 @@ void lc_entry()
 //    lc80__RemoveDuplicatesfromSortedArray_II_case();
 //    lc189__RotateArray_case();
 //    lc41__FirstMissingPositive_case();
-    lc299__BullsandCows_case();
+//    lc299__BullsandCows_case();
+    lc134__GasStation_case();
 }
